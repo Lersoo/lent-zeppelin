68be1a593d0e7a4c8a1bfd8f7056ca3fa83088b5
David <davidobob@laposte.net>
2020-02-24 18:27:25 +0100

    added new zeppelin form and sheetstyle

--- app/controllers/zeppelins_controller.rb
+++ app/controllers/zeppelins_controller.rb
@@ -1,5 +1,5 @@
 class ZeppelinsController < ApplicationController
-  before_action :set_zeppelin
+  before_action :set_zeppelin, only: %i[show]
 
   def index
     @zeppelins = Zeppelin.all
@@ -9,15 +9,23 @@ class ZeppelinsController < ApplicationController
   end
 
   def new
+    @zeppelin = Zeppelin.new
   end
 
   def create
+    @zeppelin = Zeppelin.new(zeppelin_params)
+    @zeppelin.save!
+    redirect_to zeppelins_path
   end
 
   private
 
   def set_zeppelin
     @zeppelin = Zeppelin.find(params[:id])
+  end
+  # setting strong params
+  def zeppelin_params
+    params.require(:zeppelin).permit(:name, :location, :price, :start_date, :end_date)
   end
 
 end
