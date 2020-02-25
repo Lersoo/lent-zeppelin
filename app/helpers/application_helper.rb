ec908c190cdfee10b8c516aea5f44a7dced8f967
MathLebl <leblond.mathieu.kesemale@gmail.com>
2020-02-25 16:29:22 +0100

    echec photo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_photo(user, size=40)
+    if user.photo.attached?
+      user.photo.variant(resize: "#{size}x#{size}!")
+    else
+      gravatar_image_url(user.email, size: size)
+    end
+  end
 end
