d7c672dacb452e74b686ce20739f876314ea5740
Simon Chardon <simon.chardon@yahoo.fr>
2020-02-26 17:07:54 +0100

    adding map to the zeppling show

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,14 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// add bootstrap
 import "bootstrap";
+
+//search bar auto complete
 import { addAutocomplete } from './autocomplete';
 addAutocomplete();
+
+//map box js import
+import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+import { initMapbox } from '../plugins/init_mapbox';
+initMapbox();
