f430ff65051cac3ab4ea3c902c76f07ae35cbf8f
Simon Chardon <simon.chardon@yahoo.fr>
2020-02-28 15:11:27 +0100

    finxing issu mailing

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module LentZeppelin
     config.load_defaults 6.0
 
 
-    config.action_mailer.default_url_options = { host: 'http://localhost:3000/' }
+    config.action_mailer.default_url_options = { host: 'http://lent-zeppelin.herokuapp.com' }
     config.action_mailer.asset_host = 'http://lent-zeppelin.herokuapp.com'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
