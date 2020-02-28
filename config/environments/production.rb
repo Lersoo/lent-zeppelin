acd9d0acce9a5f377de7c153dfedc4e15246cc2c
Simon Chardon <simon.chardon@yahoo.fr>
2020-02-28 15:56:16 +0100

    fixprobleme

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,8 +38,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :cloudinary
 
-    # Don't care if the mailer can't send.
-    config.action_mailer.raise_delivery_errors = 
 
   config.action_mailer.delivery_method = :smtp
   # SMTP settings for gmail
